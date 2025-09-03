1e02f56a447a0f7fa0bc3d25aa857d6ceb895757
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-04 06:58:33 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import "dotenv/config";
+import Debug from "debug";
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "@lib/auth.js";
 
+const debug = Debug("fs-auth:index");
 const app = express();
-const port = 3005;
+app.set("view engine", "pug");
+app.use(express.static("public"));
 
 // app.all("/api/auth/*", toNodeHandler(auth)); // For ExpressJS v4
 app.all("/api/auth/*splat", toNodeHandler(auth));
@@ -11,7 +15,86 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 // Mount express json middleware after Better Auth handler
 // or only apply it to routes that don't interact with Better Auth
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(morgan("dev", { immediate: true }));
+// app.use(useragent.express());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// * Endpoints
+app.get("/", async (req, res, next) => {
+  res.render("pages/index", {
+    title: "Home",
+    user: null,
+    sessions: null,
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("pages/login", {
+    title: "Login",
+  });
+});
+
+app.post("/login", async function (req, res, next) {
+  debug("@login handler - set session info");
+  console.log(req.body);
+  const email = req.body?.email ?? "";
+  const password = req.body?.password ?? "";
+
+  try {
+    const user = await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+    console.log({ user });
+    res.setHeader("HX-Redirect", "/");
+    res.send(`<div></div>`);
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send(err?.message ?? "Something wrong");
+    return next();
+  }
+});
+
+app.get("/signup", function (req, res) {
+  res.render("pages/signup", {
+    title: "Signup",
+  });
+});
+
+app.post("/signup", async function (req, res, next) {
+  console.log(req.body);
+  const name = req.body?.name ?? "";
+  const email = req.body?.email ?? "";
+  const password = req.body?.password ?? "";
+  const passwordConfirm = req.body?.passwordConfirm ?? "";
+
+  if (password !== passwordConfirm) {
+    res.status(401).send("Passwords not matched.");
+    return next(); // If I don't return, the function will continue executing.
+  }
+
+  try {
+    await auth.api.signUpEmail({
+      body: {
+        email,
+        password,
+        name,
+      },
+    });
+    // await createUser(name, email, password);
+    res.setHeader("HX-Redirect", "/login");
+    res.send(`<div></div>`);
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send(err?.message ?? "Something wrong");
+    return next();
+  }
+});
+
+// * Running app
+const PORT = 5001;
+app.listen(PORT, async () => {
+  debug(`Listening on port ${PORT}: http://localhost:${PORT}`);
 });
